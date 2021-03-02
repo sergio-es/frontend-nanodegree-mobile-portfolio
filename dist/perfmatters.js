156b98a54d1b5da0a8a4fc1f3bd052382bd02565
Sergio Esquivel <sergio.esquivel@insperity.com>
2021-03-02 16:03:33 -0600

    added dist files - removed from gitignore

---
+++ dist/perfmatters.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";window.addEventListener("load",(function(t){var n,o,e;o=(n=window.performance.timing).domContentLoadedEventStart-n.domLoading,e=n.domComplete-n.domLoading,document.getElementById("crp-stats").textContent="DCL: "+o+"ms, onload: "+e+"ms"}))})();
